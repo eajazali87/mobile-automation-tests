8e02be5253704bdef69a70a47a5e611616020f16
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-01 14:55:12 -0700

    test

--- src/test/java/componentsTest/TypographyTest.java
+++ src/test/java/componentsTest/TypographyTest.java
@@ -26,7 +26,7 @@ public class TypographyTest {
 		cap.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
 		cap.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"));
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.4");
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6");
 		cap.setCapability("appiumVersion", "1.3.4");
 		cap.setCapability(CapabilityType.BROWSER_NAME, "safari");
 		driver = new IOSDriver(sauceUrl,cap);	
