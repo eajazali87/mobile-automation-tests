b3c8bf3058b5b3b197f864f0a20258ae594a3c84
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-01 13:45:10 -0700

    test

--- src/test/java/componentsTest/TypographyTest.java
+++ src/test/java/componentsTest/TypographyTest.java
@@ -16,21 +16,25 @@ public class TypographyTest {
 	IOSDriver driver;
 	
 	@Test
-	public void mobileTest() throws MalformedURLException{
+	public void mobileTest() throws MalformedURLException, InterruptedException{
 		
-		final String USERNAME = "eajaz";
-		final String ACCESS_KEY = "dee10cb9-6c0f-4ce1-b3b4-ff7d2854ee80";
+		final String USERNAME = System.getenv("SAUCE_USERNAME");
+		final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+		
 		URL sauceUrl = new URL("http://" + USERNAME + ":"+ ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub");
 		
 		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
+		cap.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"));
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1");
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
 	    cap.setCapability("appiumVersion", "1.3.4");
 	    
-		cap.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		cap.setCapability(CapabilityType.BROWSER_NAME, "safari");
 		driver = new IOSDriver(sauceUrl,cap);
 		
-		driver.get("https://console.pearson.com/signin");
+		driver.get("http://localhost:8000/src/main/java/fixtures/index.html");
+		Thread.sleep(5000);
 		
 	    driver.close();
 		driver.quit();
