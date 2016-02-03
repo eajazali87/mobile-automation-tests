399f6cf6522ccbc8d334692275a382d1b2e7e1b5
Eajaz Ali Mahaboob Basha <eajazali.mahaboobbasha@pearson.com>
2016-02-02 19:01:44 -0700

    test

--- src/test/java/componentsTest/TypographyTest.java
+++ src/test/java/componentsTest/TypographyTest.java
@@ -33,7 +33,7 @@ public class TypographyTest {
 		cap=new DesiredCapabilities();
 		cap.setCapability("tunnel-identifier", System.getenv("TRAVIS_JOB_NUMBER"));
 		cap.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"));
-		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.1");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.3");
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Nexus 7C Emulator");
 		cap.setCapability("appiumVersion", "1.4");
 		cap.setCapability(CapabilityType.BROWSER_NAME, "chrome");
